8c2f7a460ff4397ac1ba2347971df4a5d58026c3
Stanislav Mitev <stanitev@gmail.com>
2023-03-31 13:09:44 +0300

    add everything together

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from myturtle import CreateTurtle
+from car import Car
+import time
 
 # Create and modify the screen
 screen = Screen()
@@ -8,15 +10,22 @@ screen.title("Turtle Crossing the Road")
 screen.tracer(0)
 
 # Create objects
-timmy = CreateTurtle()
+player = CreateTurtle()
+car_manager = Car()
 
 # Variables
 game_is_on = True
 
+# Create a movement
 screen.listen()
-screen.onkey(fun=timmy.move, key="Up")
+screen.onkey(fun=player.move_up, key="Up")
 
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+
+    car_manager.create_cars()
+    car_manager.move_cars()
+
 
 screen.exitonclick()
