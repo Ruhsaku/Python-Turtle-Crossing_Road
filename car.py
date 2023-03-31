cfdc2d87c80814b96bb8d11473df51e6dfa603d0
Stanislav Mitev <stanitev@gmail.com>
2023-03-31 14:08:05 +0300

    include speed increasement method

--- car.py
+++ car.py
@@ -9,23 +9,21 @@ MOVE_INCREMENT = 10
 class Car:
     def __init__(self):
         self.all_cars = []
+        self.car_speed = STARTING_MOVE_DISTANCE
 
     def create_cars(self):
-        random_chance = random.randint(1, 6)
+        random_chance = random.randint(1, 5)
         if random_chance == 1:
             new_car = Turtle("square")
             new_car.penup()
-            new_car.setheading(180)
             new_car.color(random.choice(COLORS))
             new_car.shapesize(stretch_wid=1, stretch_len=2)
             new_car.goto((400, random.randint(-230, 230)))
-            new_car.x_move = 0.25
             self.all_cars.append(new_car)
 
     def move_cars(self):
         for car in self.all_cars:
-            car.forward(STARTING_MOVE_DISTANCE)
+            car.backward(self.car_speed)
 
-
-
-
+    def level_up(self):
+        self.car_speed += MOVE_INCREMENT
