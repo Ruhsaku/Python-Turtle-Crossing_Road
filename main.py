eee23b6be351549ac82d10e1b8a8124a5c8cab3b
Stanislav Mitev <stanitev@gmail.com>
2023-03-31 14:09:45 +0300

    combine everything together

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
-from myturtle import CreateTurtle
+from player import Player
 from car import Car
+from scoreboard import Scoreboard
 import time
 
 # Create and modify the screen
@@ -10,11 +11,13 @@ screen.title("Turtle Crossing the Road")
 screen.tracer(0)
 
 # Create objects
-player = CreateTurtle()
+player = Player()
 car_manager = Car()
+scoreboard = Scoreboard()
 
 # Variables
 game_is_on = True
+car_player_dist = 22
 
 # Create a movement
 screen.listen()
@@ -26,6 +29,18 @@ while game_is_on:
 
     car_manager.create_cars()
     car_manager.move_cars()
+
+    # Detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < car_player_dist:
+            scoreboard.game_over()
+            game_is_on = False
+
+    # Detect successful crossing
+    if player.is_at_finish_line():
+        player.go_to_start()
+        scoreboard.increase_level()
+        car_manager.level_up()
 
 
 screen.exitonclick()
