ea28097a1a57128d18bfe9b2fbc7e2ed3a306208
Stanislav Mitev <stanitev@gmail.com>
2023-03-31 14:09:13 +0300

    add finishing methods

--- player.py
+++ player.py
@@ -1,14 +1,26 @@
 from turtle import Turtle
 
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE = 280
 
-class CreateTurtle(Turtle):
+
+class Player(Turtle):
     def __init__(self):
         super().__init__()
         self.shape("turtle")
         self.penup()
         self.setheading(90)
-        self.goto(0, -280)
-        self.y_move = 25
+        self.go_to_start()
 
     def move_up(self):
-        self.forward(10)
+        self.forward(MOVE_DISTANCE)
+
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
+
+    def is_at_finish_line(self):
+        if self.ycor() > FINISH_LINE:
+            return True
+        else:
+            return False
